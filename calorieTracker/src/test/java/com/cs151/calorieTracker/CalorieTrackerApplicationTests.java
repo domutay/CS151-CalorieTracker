b02bd865bee70cea0e43348c6a61eb7096d09c73
BhagyeshRathi <63847327+bhagyeshrathi07@users.noreply.github.com>
2022-11-25 15:22:25 -0800

    Created Dynamodb database and it's CRUD API
    Added controller, repository, and configuration for users

---
+++ calorieTracker/src/test/java/com/cs151/calorieTracker/CalorieTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs151.calorieTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
