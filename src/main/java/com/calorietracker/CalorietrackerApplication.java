ee348ae0e88bc494fd8bd9d2c6a4d38d154efa84
domlabutay <d.labutay@gmail.com>
2022-11-26 00:16:23 -0800

    started mongodb registration/login integration

---
+++ src/main/java/com/calorietracker/CalorietrackerApplication.java
@@ -0,0 +1,14 @@
+package com.calorietracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories
+public class CalorietrackerApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorietrackerApplication.class, args);
+	}
+}
---
+++ src/main/java/com/calorietracker/controllers/RegisterController.java
@@ -0,0 +1,37 @@
+package com.calorietracker.controllers;
+
+import com.calorietracker.dto.UserDto;
+import com.calorietracker.model.User;
+import com.calorietracker.service.IUserService;
+import com.calorietracker.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@EnableMongoRepositories
+public class RegisterController {
+    @Autowired
+    private UserService userService = new UserService();
+    @GetMapping("/register")
+    public String register(Model model) {
+        UserDto userDto = new UserDto();
+        model.addAttribute("user", userDto);
+        return "register";
+    }
+    @PostMapping("/home")
+    public String registerSubmit(@ModelAttribute UserDto userDto, Model model) {
+        model.addAttribute("user", userDto);
+        userService.registerNewUserAccount(userDto);
+        return "home";
+    }
+}
